376f24877cf993b8fb13f89630b64dbc2e6492ea
bilelkhalsi <bilel_khalsi@live.fr>
2023-04-04 05:16:41 -0400

    Ngx translate integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { EnvironmentModule } from "@rex/core";
 import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
@@ -11,19 +14,33 @@ import { HorreaMaterialModule } from './material/horrea-material.module';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { AppStoreModule } from './store/app-store.module';
 
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     AppStoreModule,
     BrowserAnimationsModule,
     HorreaMaterialModule,
     AppBarModule,
     FooterModule,
-    EnvironmentModule.forRoot(environment)
+    EnvironmentModule.forRoot(environment),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
