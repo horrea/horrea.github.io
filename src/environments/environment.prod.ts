626bb0940ac299bfa0e2ae544ac94a1099bb2834
bilelkhalsi <bilel_khalsi@live.fr>
2023-05-17 03:44:33 -0400

    firebase integrated to app module

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,14 @@ export const environment: Environment = {
   githubClientId: 'ecce8a29672656f7e997',
   githubLoginUrl: 'https://github.com/login/oauth/authorize',
   githubTokenUrl: 'https://github.com/login/oauth/access_token',
-  githubScopes: 'user:email'
+  githubScopes: 'user:email',
+  firebase: {
+    apiKey: "AIzaSyCo73mbNIJ9s-qtC7DHB3uawBSCo-6U1KM",
+    authDomain: "horrea-2e5b4.firebaseapp.com",
+    projectId: "horrea-2e5b4",
+    storageBucket: "horrea-2e5b4.appspot.com",
+    messagingSenderId: "504252915181",
+    appId: "1:504252915181:web:a694a922545a8cf31c5be4",
+    measurementId: "G-Q487FR9JW6"
+  }
 };
